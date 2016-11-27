27d9ca39e537f31bdb2a1be93b424f444f8df7e5
M Ainur Rofik <samarqhandi17@gmail.com>
2016-11-27 12:37:27 -0800

    Update ExampleInstrumentedTest.java

--- sim-kelas/app/src/androidTest/java/it/yudharta/simkelas/ExampleInstrumentedTest.java
+++ sim-kelas/app/src/androidTest/java/it/yudharta/simkelas/ExampleInstrumentedTest.java
@@ -1,12 +1,9 @@
 package it.yudharta.simkelas;
-
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.*;
 
 /**
@@ -20,7 +17,6 @@ public class ExampleInstrumentedTest {
     public void useAppContext() throws Exception {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
-
         assertEquals("it.yudharta.simkelas", appContext.getPackageName());
     }
 }
